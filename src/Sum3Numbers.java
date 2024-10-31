4be4652dc384a6b026157ff95c152def7bde527c
Samkingston404 <rsamuelvivek900@gamil.com>
2024-10-31 19:05:33 +0530

    sum of three numbers

--- src/Sum3Numbers.java
+++ src/Sum3Numbers.java
@@ -1,8 +1,8 @@
 public class Sum3Numbers {
     public static void main(String[] args) {
         int number1 = 1;
-        int number2 = 2;
-        int number3 = 3;
+        int number2 = 1;
+        int number3 = 1;
         int sum = number1 + number2 + number3;
         System.out.println(sum);
     }
