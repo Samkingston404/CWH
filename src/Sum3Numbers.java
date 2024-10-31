d6eeda1516e5b1a9f17d12615704469023a63ebb
Samkingston404 <rsamuelvivek900@gamil.com>
2024-10-31 19:04:26 +0530

    Added my name to Hello World Program

---
+++ src/Sum3Numbers.java
@@ -0,0 +1,9 @@
+public class Sum3Numbers {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 2;
+        int number3 = 3;
+        int sum = number1 + number2 + number3;
+        System.out.println(sum);
+    }
+}
