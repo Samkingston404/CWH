83d24a52431cc49e1aa15f23266721c02ab0ef3b
Samkingston404 <rsamuelvivek900@gamil.com>
2024-10-31 22:33:15 +0530

    Taking input from user

---
+++ src/CalculatePercentage.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CalculatePercentage {
+    public static void main(String[] args) {
+        Scanner in    = new Scanner(System.in);
+        int telugu    = in.nextInt();
+        int hindi     = in.nextInt();
+        int english   = in.nextInt();
+        int maths     = in.nextInt();
+        int science   = in.nextInt();
+        int total     = telugu+hindi+english+maths+science;
+        float percentage= (total / 500.0f) * 100;
+        System.out.println(percentage);
+    }
+}
