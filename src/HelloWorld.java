508003d4217878dcadeea989fc577a4e67602bb2
Samkingston404 <rsamuelvivek900@gamil.com>
2024-10-31 16:56:25 +0530

    Added my name to Hello World Program

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Enter your code here !
+        System.out.print("Hello World! ");
+        System.out.println("This is Sam.");
+    }
+}
