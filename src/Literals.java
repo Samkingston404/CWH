628560924301394a4ee3f74af85aad295445d0d0
Samkingston404 <rsamuelvivek900@gamil.com>
2024-10-31 20:12:19 +0530

    adddes values to variables with data types

---
+++ src/Literals.java
@@ -0,0 +1,13 @@
+public class Literals {
+    public static void main(String[] args) {
+        byte age   = 127;
+        short age2 = 1234;
+        int age3   = 1234567899;
+        long age4  = 1234567891234567L;
+        float f    = 1.23f;
+        double d   = 1.23456d;
+        char ch    = 'A';
+        String S   = "Sam";
+        boolean b  = true;
+    }
+}
