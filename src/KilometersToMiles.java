62883bf304f4907d4d38936a55d82fa0c05cd32c
Samkingston404 <rsamuelvivek900@gamil.com>
2024-11-01 13:58:22 +0530

    Km To Miles (km * 0.621371)

---
+++ src/KilometersToMiles.java
@@ -0,0 +1,8 @@
+public class KilometersToMiles {
+    public static void main(String[] args) {
+        float km     = 4.5f;
+        float mile   = 0.621371f;
+        float answer = km * mile;
+        System.out.printf("%.2f",answer);
+    }
+}
