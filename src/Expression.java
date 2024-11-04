9c656ebde2b2f51e22bf4a7c726f5df8ce2edc61
Samkingston404 <rsamuelvivek900@gamil.com>
2024-11-04 17:58:34 +0530

    v2-u2-2as

---
+++ src/Expression.java
@@ -0,0 +1,10 @@
+public class Expression {
+    public static void main(String[] args) {
+        int v = 8;
+        int u = 2;
+        int a = 2;
+        int s = 2;
+        int answer = (v*v - u*u) / (2*a*s);
+        System.out.println(answer);
+    }
+}
