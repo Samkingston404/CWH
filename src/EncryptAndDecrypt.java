44fe45873dc0b4d87efd0e852bf74d2df6d87d43
Samkingston404 <rsamuelvivek900@gamil.com>
2024-11-04 17:23:23 +0530

    Explicit int to char type convention

--- src/EncryptAndDecrypt.java
+++ src/EncryptAndDecrypt.java
@@ -1,7 +1,7 @@
 public class EncryptAndDecrypt {
     public static void main(String[] args) {
         char ch = 'A';
-        ch = (char) (ch + 8);
+        ch = (char) (ch + 8 );
 
         System.out.println(ch);
         ch = (char) (ch - 8);
